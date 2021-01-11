dc7021e83e566a2d960ed70b567e3fc7a4ccf295
kacemws <berras.belkacem@gmail.com>
2021-01-11 11:59:34 +0100

    schema for app model

---
+++ src/models/AppSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const appSchema = new mongoose.Schema({
+  appName: {
+    type: String,
+    required: [true, "App's name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  publicKey: {
+    type: String,
+    required: [true, "Public Key is required"],
+  },
+  privateKey: {
+    type: String,
+    required: [true, "Private Key is required"],
+  },
+  secretKey: {
+    type: String,
+    required: [true, "Secret Key is required"],
+  },
+  created: {
+    type: Date,
+    required: [true, "Created date is required"],
+  },
+});
+
+module.exports = appSchema;
